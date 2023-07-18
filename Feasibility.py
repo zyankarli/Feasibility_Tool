ab33fab990d3b079afd6daa58b8f3abf5a77dbd8
zyankarli <scheifinger.karl@hotmail.com>
2023-07-18 15:10:58 +0200

    update graph
    show new graph with correct data

--- Feasibility.py
+++ Feasibility.py
@@ -24,96 +24,146 @@ st.markdown(hide_default_format, unsafe_allow_html=True)
 def get_data():
     #get IIASA identification
     #IIASA
-    #iiasa_creds = r"C:\Users\scheifinger\Documents\GitHub\Feasibility_Tool\iiasa_credentials.yml" 
+    iiasa_creds = r"C:\Users\scheifinger\Documents\GitHub\Feasibility_Tool\iiasa_credentials.yml" 
     #Home
     #iiasa_creds = r"C:\Users\schei\OneDrive\Dokumente\GitHub\Feasibility_Tool\iiasa_credentials.yml"
     #iiasa_creds = st.secrets['iiasa_creds']
-    pyam.iiasa.set_config(st.secrets['iiasa_creds']['username'], st.secrets['iiasa_creds']['password'])
-    pyam.iiasa.Connection()
+    #pyam.iiasa.set_config(st.secrets['iiasa_creds']['username'], st.secrets['iiasa_creds']['password'])
+    #pyam.iiasa.Connection()
 
     #connections = list(pyam.iiasa.Connection(creds=iiasa_creds).valid_connections)
     #query for climate scenario data
     df = pyam.read_iiasa(
         name = 'engage_internal',
-        #creds = iiasa_creds,
-        scenario =['T34_550_feas_em',
-                   'T34_550_feas_pr',
-                   'T34_550_feas_ref',
-                   'T34_1000_feas_em',
-                   'T34_1000_feas_pr',
-                   'T34_1000_feas_ref'],
-        variable=["Emissions|CO2", "Primary Energy|Coal", "Primary Energy|Coal|w/ CCS", "Final Energy|Transportation", "Carbon Capture"],
-        region=['World']
-    )   
+        creds = iiasa_creds,
+        scenario =[
+            "T34_1000_ref",
+            "T34_1000_govem",
+            "T34_1000_feas_em",
+            "T34_1000_bitb_em",
+            "T34_1000_bitb_ref",
+            "T34_1000_enab_em"
+        ],
+        variable=["Emissions|CO2", 
+                  'Emissions|Kyoto Gases',
+                  'Primary Energy|Coal',
+                  'Primary Energy|Gas',
+                  'Primary Energy',
+                  'Capacity|Electricity|Solar',
+                  'Capacity|Electricity|Wind',
+                  'Carbon Sequestration|CCS',
+                  'Carbon Sequestration|Land Use'
+                  ],
+        region=["World",
+                "North America; primarily the United States of America and Canada",
+                "Eastern and Western Europe (i.e., the EU28)",
+                "Countries of centrally-planned Asia; primarily China",
+                "Countries of South Asia; primarily India", 
+                "Countries of Sub-Saharan Africa",
+                "Countries of Latin America and the Caribbean",
+                "Countries of the Middle East; Iran, Iraq, Israel, Saudi Arabia, Qatar, etc."]
+    )      
+
     #return data format of df
     return df
 
 df = get_data().data
 
-#Data wrangling
+#DATA WRANGLING
+#get regional groupings
+## OECD: "North America; primarily the United States of America and Canada","Eastern and Western Europe (i.e., the EU28)"
+#TODO Clarify that countries of Asia and Latin America are missing
+## China:  Countries of centrally-planned Asia; primarily China
+## Rest of the world: other countries
+#get world
+world = df[df['region'] == "World"]
+world.loc[:, "region"] = "World"
+#get OECD*
+oecd = df[df["region"].isin(["North America; primarily the United States of America and Canada","Eastern and Western Europe (i.e., the EU28)"])]\
+    .groupby(["model", "scenario", "variable", "year", "unit"])\
+        .agg({"value": "sum"}).reset_index()
+oecd['region'] = "OECD"
+#get China
+china = df[df['region'] == "Countries of centrally-planned Asia; primarily China"]
+china.loc[:, "region"] = "China"
+#get RoW
+row = df[~df["region"].isin(["North America; primarily the United States of America and Canada","Eastern and Western Europe (i.e., the EU28)", "Countries of centrally-planned Asia; primarily China"])]\
+        .groupby(["model", "scenario", "variable", "year", "unit"])\
+            .agg({"value": "sum"}).reset_index()
+row["region"] = "RoW"
+#Concat four regions
+df = pd.concat([world, oecd, china, row]).reset_index()
+
 #from long to wide
 df = pd.pivot(data=df, index=['model','scenario', 'region', 'year'], columns = 'variable', values = 'value').reset_index()
-#get different carbon budgets from scenario name
-df['carbon_budget'] = np.where(df['scenario'].str.contains("1000", case=False), "2C", "1.5C")
 #get diffeernt scenario narratives from scenario name
-df['scenario_narrative'] = np.where(df['scenario'].str.contains("_em", case=True), "EM", np.nan) 
-df['scenario_narrative'] = np.where(df['scenario'].str.contains("_ref", case=True), "REF", df['scenario_narrative']) 
-df['scenario_narrative'] = np.where(df['scenario'].str.contains("_pr", case=True), "PR", df['scenario_narrative'])
+df['scenario_narrative'] = np.where(df['scenario'].str.contains("T34_1000_ref", case=True), "Cost Effective", np.nan) 
+df['scenario_narrative'] = np.where(df['scenario'].str.contains("T34_1000_bitb_ref", case=True), "Tech", df['scenario_narrative']) 
+df['scenario_narrative'] = np.where(df['scenario'].str.contains("T34_1000_govem", case=True), "Instit", df['scenario_narrative'])
+df['scenario_narrative'] = np.where(df['scenario'].str.contains("T34_1000_bitb_em", case=True), "Tech+Inst", df['scenario_narrative'])
+df['scenario_narrative'] = np.where(df['scenario'].str.contains("T34_1000_enab_em", case=True), "Inst+Enab", df['scenario_narrative'])
+df['scenario_narrative'] = np.where(df['scenario'].str.contains("T34_1000_feas_em", case=True), "Tech+Inst+Enab", df['scenario_narrative'])
 
-#NET ZERO
-# calculate year that each scenario hit's net zero
-netzero_df = df[df["Emissions|CO2"] <= 0].groupby(["model", "scenario", "carbon_budget"])['year'].min().reset_index()
-netzero_df.rename(columns={"year": "year_netzero"}, inplace=True)
-
-#COAL PHASEOUT
-# get amounts of unabated coal
-#both Coal data are in EJ/yr
-unabcoal_df = df[["model", "scenario", "carbon_budget", "region","year", "Primary Energy|Coal", "Primary Energy|Coal|w/ CCS"]] 
-unabcoal_df["Unabated Coal"] = unabcoal_df["Primary Energy|Coal"] - unabcoal_df["Primary Energy|Coal|w/ CCS"]
-#pick threshold for when coal is supposed to the phased out // now it's 8EJ/y
-unabcoal_df = unabcoal_df[unabcoal_df["Unabated Coal"] <= 8].groupby(["model", "scenario", "carbon_budget"])['year'].min().reset_index()
-unabcoal_df.rename(columns={"year": "year_netcoal"}, inplace=True)
-
-#TRANSPORT ENERGY REDUCTION
-#calculate change in transportation energy from 2020 to 2030
-tranred_df = df[["model", "scenario", "carbon_budget", "region","year", "Final Energy|Transportation"]]
-#filter for year 2020 & 2030
-tranred_df = tranred_df[(tranred_df["year"] == 2020) | (tranred_df["year"] == 2030) ].reset_index().drop("index", axis=1)
-#from long to wide
-tranred_df = pd.pivot(data=tranred_df, index=["model", "scenario", "carbon_budget", "region"], 
-             columns="year", values= "Final Energy|Transportation").reset_index()
-#Finale Energy Transportation = (FET)
-tranred_df["redu_FET"] = (tranred_df[2030] - tranred_df[2020]) / tranred_df[2020] 
+#caluclate percentages of energymix for coal and gas
+df["Share_Coal"] = df["Primary Energy|Coal"] / df["Primary Energy"]
+df["Share_Gas"] = df["Primary Energy|Gas"] / df["Primary Energy"]
 
 
-#MERGE
-to_plot = pd.merge(left=netzero_df, right=unabcoal_df, on=["model", "scenario", "carbon_budget"])
-to_plot = pd.merge(left=to_plot, right=tranred_df.loc[:, ~tranred_df.columns.isin([2020, 2030])],
-                   on=["model", "scenario", "carbon_budget"])
+#GET DATA TO PLOT
+to_plot_df = df[(df['year'].isin([2020, 2030, 2040])) & (df["scenario"].isin(["T34_1000_ref", "T34_1000_govem"])) & (df["region"] == "World")]
 
-#FIRST figure - CO2 trajectories of different scenario narratives
-fig1 = px.line(df,
-               x = "year",
-               y="Emissions|CO2",
-               color = "carbon_budget",
-               line_dash="model",
-               facet_col="scenario_narrative", 
-               title = "CO2 reduction trajectories per scenario narrative",
-               color_discrete_sequence=px.colors.qualitative.G10,
-               hover_data = {'scenario':True, 'model':True})
+#calculate reductions
+reductions_df = to_plot_df[["model", "scenario", "region", 'year', "Emissions|CO2"]]
+reductions_df = pd.pivot(data=reductions_df, index=reductions_df.drop(['year', "Emissions|CO2"], axis=1).columns, columns = 'year', values = 'Emissions|CO2').reset_index()
+reductions_df["2030_CO2_redu"] = ( (reductions_df[2030] - reductions_df[2020]) / reductions_df[2020]) * -1 #multiplied with -1 to have positive values 
+reductions_df["2040_CO2_redu"] = ((reductions_df[2040] - reductions_df[2020]) / reductions_df[2020]) * -1 
+reductions_df.drop([2020, 2030, 2040], axis=1, inplace=True)
+
+#merge
+to_plot_df = pd.merge(left=to_plot_df, right=reductions_df, on=["model", "scenario", "region"])
+
+#FIGURE 
+fig = go.Figure(px.strip(
+    to_plot_df[(to_plot_df['year'] == 2030)],
+    x='scenario_narrative',
+    y='2030_CO2_redu',
+    color='model',
+    stripmode='overlay'))
 
 
+fig.add_trace(go.Box(
+    y = to_plot_df[(to_plot_df['year'] == 2030) & (to_plot_df["scenario_narrative"] == "Instit")]["2030_CO2_redu"],
+    name = "Instit",
+    marker_color='grey',
+    opacity=0.3,
+    boxpoints=False,
+    showlegend=False
+))
 
-#SECOND figure
+fig.add_trace(go.Box(
+    y = to_plot_df[(to_plot_df['year'] == 2030) & (to_plot_df["scenario_narrative"] == "Cost Effective")]["2030_CO2_redu"],
+    name = "Cost Effective",
+    marker_color='grey',
+    opacity=0.3,
+    boxpoints=False,
+    showlegend=False
+))
 
-fig2 = px.scatter(
-    data_frame=to_plot,
-    x = "year_netcoal",
-    y="redu_FET", 
-    color = "carbon_budget",
-    title = "Example of scenario space",
-    color_discrete_sequence=px.colors.qualitative.G10
-)
+fig.update_traces({'marker':{'size': 8}})
+
+fig.update_layout(autosize=False,
+                  width=600,
+                  height=600,
+                  title = go.layout.Title(
+                    text="ENGAGE 2C scenarios <br><sup>Global CO2 reductions by 2030</sup>",
+                    xref="paper",
+                    x=0),
+                  xaxis_title="Scenario Narrative",
+                  yaxis_title = "CO2 reductions",
+                  yaxis_tickformat='.0%')
+    
+st.plotly_chart(fig, theme="streamlit")
+
 
 
 #STATEMENT
@@ -143,8 +193,3 @@ else:
     col1, col2, col3 = st.columns(3)
     col1.metric("Earliest possible net-zero year:", earliest_net_zero_year)
     col2.metric("Is it possible to achieve the 2014 PA? ", PA_aligned)
-
-
-#Print graphs
-st.plotly_chart(fig1, theme="streamlit")
-st.plotly_chart(fig2, theme="streamlit")
